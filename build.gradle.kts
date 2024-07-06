f7182f5d16792f29ea1ff18385c46c89595cd033
Kamran Qurbanlı <k.qurbanli@gmail.com>
2024-07-06 16:03:49 +0300

    Push SDK changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	alias(libs.plugins.android.application)
+	id("com.android.library")
 	alias(libs.plugins.jetbrains.kotlin.android)
 }
 
@@ -8,11 +8,8 @@ android {
 	compileSdk = 34
 
 	defaultConfig {
-		applicationId = "com.kamran.energonsdk"
 		minSdk = 24
-		targetSdk = 34
-		versionCode = 1
-		versionName = "1.0"
+		lint.targetSdk = 34
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
--- src/main/java/com/kamran/energonsdk/Animal.kt
+++ src/main/java/com/kamran/energonsdk/Animal.kt
@@ -2,5 +2,6 @@ package com.kamran.energonsdk
 
 data class Animal(
 	val a:Int = 1,
-	val name:String = String()
+	val name:String = String(),
+	val genus:String = String()
 )
